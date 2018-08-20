153a1dd47e18a7e5eb66c41e106b55373ff43b75
ros-diana <sarahtambunan24@gmail.com>
2018-08-20 16:54:47 +0700

    test

--- backend/views/layouts/leftside.php
+++ backend/views/layouts/leftside.php
@@ -16,8 +16,8 @@ use yii\helpers\Url;
                     'items' => [
                         ['label' => 'Menu', 'options' => ['class' => 'header']],
                         ['label' => 'Beranda', 'icon' => 'fa fa-home', 'url' => ['/site']],
-                        ['label' => 'Kelas Reguler', 'icon' => 'fa fa-users', 'url' => ['/siswa']],
-                        ['label' => 'Kelas Intensif', 'icon' => 'fa fa-user', 'url' => ['/siswa']], 
+                        ['label' => 'Kelas Reguler', 'icon' => 'fa fa-users', 'url' => ['/']],
+                        ['label' => 'Kelas Intensif', 'icon' => 'fa fa-user', 'url' => ['/']], 
                         ['label' => 'Akun user', 'icon' => 'fa fa-user', 'url' => ['/siswa']],
                         ['label' => 'Penjadwalan', 'icon' => 'fa fa-calendar', 'url' => ['/jadwal']],
                         ['label' => 'Daftar Ruangan', 'icon' => 'fa fa-list', 'url' => ['/ruangan']],
